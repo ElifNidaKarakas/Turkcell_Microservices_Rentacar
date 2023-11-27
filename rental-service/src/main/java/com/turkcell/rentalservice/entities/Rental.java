0a5910601d7b71b1558392ce393e2d4766c31612
@baranbuyuk <baran.buyuk@kolaysoft.com.tr>
2023-11-27 23:16:38 +0300

    refactor: rental entity revised

--- rental-service/src/main/java/com/turkcell/rentalservice/entities/Rental.java
+++ rental-service/src/main/java/com/turkcell/rentalservice/entities/Rental.java
@@ -10,11 +10,15 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "rentals")
 public class Rental {
-
     @Id
     @Column(name = "car_id")
     private String carId;
 
+    @Column(name = "customer_id")
+    private int customerId;
+
     @Column(name = "car_status")
     private String carStatus;
+
+
 }
